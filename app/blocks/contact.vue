77a9f30f3b9cd5e3dc8e03327f3068338b35c982
Christian Mizon <hello@christianmizon.co.uk>
2025-03-10 15:52:40 +0000

    Add test Netlify form

--- app/blocks/contact.vue
+++ app/blocks/contact.vue
@@ -53,6 +53,18 @@
 					</svg>
 				</div>
 			</h4>
+
+			<form name="contact" netlify>
+				<p>
+					<label>Name <input type="text" name="name" /></label>
+				</p>
+				<p>
+					<label>Email <input type="email" name="email" /></label>
+				</p>
+				<p>
+					<button type="submit">Send</button>
+				</p>
+			</form>
 		</container>
 	</section>
 </template>
